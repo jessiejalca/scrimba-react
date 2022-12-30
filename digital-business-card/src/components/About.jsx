1d0ef6583bd938f8a4416abbf1d972602d0351a2
Jessie Jalca <jessiejalca@gmail.com>
2022-12-29 22:48:12 -0500

    Edit content to Interests section

--- digital-business-card/src/components/About.jsx
+++ digital-business-card/src/components/About.jsx
@@ -1,7 +1,7 @@
 const About = () => (
   <section>
     <h2>About</h2>
-    <p>I'm a Digital Media major at UCF with a keen interest in frontend web development.</p>
+    <p>I'm a Digital Media major at UCF and frontend web developer.</p>
   </section>
 );
 
--- digital-business-card/src/components/Interests.jsx
+++ digital-business-card/src/components/Interests.jsx
@@ -1,7 +1,7 @@
 const Interests = () => (
   <section>
     <h2>Interests</h2>
-    <p>Things about me.</p>
+    <p>I love to travel and see new places. I'm currently learning French. I have a lot of fun making carefully tailored playlists on Spotify. I'm also a major dog person.</p>
   </section>
 );
 
