600025f6defe03d216b17798f984a5f3a4b3cbdd
Jessie Jalca <jessiejalca@gmail.com>
2022-12-29 22:50:41 -0500

    Add a little bit of extra info in About section

--- digital-business-card/src/components/About.jsx
+++ digital-business-card/src/components/About.jsx
@@ -1,7 +1,7 @@
 const About = () => (
   <section>
     <h2>About</h2>
-    <p>I'm a Digital Media major at UCF and frontend web developer.</p>
+    <p>I'm a Digital Media major at UCF and frontend web developer, with a background in design.</p>
   </section>
 );
 
