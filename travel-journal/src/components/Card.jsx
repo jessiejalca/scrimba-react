0fb8f2e14040d2f005fb6b672d0a0418f1ee3e6b
Jessie Jalca <jessiejalca@gmail.com>
2023-01-07 10:20:50 -0500

    style(card): Create layout & specify typography
    CHANGES
    Sized cover image, specified typography, and used flexbox
    and adjusted DOM and class names to create layout.

--- travel-journal/src/components/Card.jsx
+++ travel-journal/src/components/Card.jsx
@@ -4,16 +4,16 @@ const Card = () => {
   return (
     <article className="card">
       <img src="https://source.unsplash.com/WLxQvbMyfas" />
-      <div className="card--location">
-        <div className="card--country">
+      <div className="card--info">
+        <div className="card--location">
           <img src="../public/locationiconImage24px.svg" />
-          <p>Japan</p>
+          <p className="card--country">Japan</p>
+          <a className="card--link" href="">View on Google Maps</a>
         </div>
-        <p className="card--link">View on Google Maps</p>
+        <h1 className="card--title">Mount Fuji</h1>
+        <p className="card--date">12 Jan, 2021 - 24 Jan, 2021</p>
+        <p className="card--description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
       </div>
-      <h1>Mount Fuji</h1>
-      <p className="card--date">12 Jan, 2021 - 24 Jan, 2021</p>
-      <p className="card--description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
     </article>
   )
 }
